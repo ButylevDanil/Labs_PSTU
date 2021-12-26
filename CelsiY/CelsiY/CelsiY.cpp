a901e47fbf5ad97de4e926393fa5af7c118a43cc
ButylevDanil <93428736+ButylevDanil@users.noreply.github.com>
2021-12-27 04:40:42 +0500

    Add files via upload

---
+++ CelsiY/CelsiY/CelsiY.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double c, f;
+    cout << "Введите градусы по Фаренгейту" << endl;
+    cin >> f;
+    c = floor(50. / 9 * (f - 32)) / 10;
+    cout << f << " градусов по Фаренгейту = " << c << " градусам по Цельсию" << endl;
+    return 0;
+}
+
+
+
+
+
