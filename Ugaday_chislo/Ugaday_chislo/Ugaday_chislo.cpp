c01507a46aebeb4d58261a7669ec27619e89ce41
ButylevDanil <93428736+ButylevDanil@users.noreply.github.com>
2021-12-27 04:39:40 +0500

    Add files via upload

---
+++ Ugaday_chislo/Ugaday_chislo/Ugaday_chislo.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n,k,c;
+	k = 1 + rand() % 100;
+	{
+		int i = 0;
+		while (i < 7) {
+			cout << "Введите число" << endl;
+			cin >> c;
+			i++;
+			if (c > k && i < 7) {
+				cout << "Загаданное число меньше" << endl;
+			}
+			else {
+				if (c == k) {
+					cout << "Число угадано" << endl;
+					i += 7;
+				}
+				else {
+					if (i == 7) {
+						cout << "Число не угадано" << endl;
+					}
+					else {
+						cout << "Загаданное число больше" << endl;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
