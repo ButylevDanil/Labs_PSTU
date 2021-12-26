dd297da2dce134c3f2c2345c38891d0bfa4990dc
ButylevDanil <93428736+ButylevDanil@users.noreply.github.com>
2021-12-27 04:51:40 +0500

    Add files via upload

---
+++ Proizvedenie/Proizvedenie/Proizvedenie.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>	
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float x, y;
+
+	cout << "Введите два числа" << endl;
+	cin >> x >> y;
+	cout << "Их произведение равно " << x * y;
+
+	return 0;
+}
