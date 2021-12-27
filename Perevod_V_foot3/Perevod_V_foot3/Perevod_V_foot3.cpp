d36f7edba4b0e0197c23561fdf75685d0fb2218e
ButylevDanil <93428736+ButylevDanil@users.noreply.github.com>
2021-12-27 05:05:09 +0500

    Add files via upload

---
+++ Perevod_V_foot3/Perevod_V_foot3/Perevod_V_foot3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float g;
+
+	cout << "Введите объём в галлонах" << endl;
+	cin >> g;
+	cout << "Объём в кубических футах равен " << g / 7.481;
+
+	return 0;
+}
